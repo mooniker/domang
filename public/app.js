ffe3c4092b23dc3bebbf837826136ac58120f082
James Moon <mooniker@gmail.com>
2016-02-10 22:43:45 -0500

    Add custom bus stop icon

--- public/app.js
+++ public/app.js
@@ -22,6 +22,41 @@
       });
     }
 
+    var local_icons = {
+      default_icon: {},
+      bus_stop_icon: {
+        iconUrl: '/icons/busstop.png',
+        iconSize: [32, 37],
+        iconAnchar: [16, 1],
+      }
+
+      // leaf_icon: {
+      //   iconUrl: 'examples/img/leaf-green.png',
+      //   shadowUrl: 'examples/img/leaf-shadow.png',
+      //   iconSize:     [38, 95], // size of the icon
+      //   shadowSize:   [50, 64], // size of the shadow
+      //   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      //   shadowAnchor: [4, 62],  // the same for the shadow
+      //   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      // },
+      // div_icon: {
+      //   type: 'div',
+      //   iconSize: [230, 0],
+      //   html: 'Using <strong>Bold text as an icon</strong>: Lisbon',
+      //   popupAnchor:  [0, 0]
+      // },
+      // orange_leaf_icon: {
+      //   iconUrl: 'examples/img/leaf-orange.png',
+      //   shadowUrl: 'examples/img/leaf-shadow.png',
+      //   iconSize:     [38, 95],
+      //   shadowSize:   [50, 64],
+      //   iconAnchor:   [22, 94],
+      //   shadowAnchor: [4, 62]
+      // }
+    };
+
+    $scope.icons = local_icons;
+
     function centerChange() {
       map.updateNearbyBusStops();
     }
@@ -41,7 +76,9 @@
         draggable: false,
         clickable: true,
         keyboard: true,
-        riseOnHover: true
+        riseOnHover: true,
+        message: stopName,
+        icon: local_icons.bus_stop_icon
       }
     }
 
