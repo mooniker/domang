e9daa9228f8f2edd40d0c9159886b818e60518bf
James Moon <mooniker@gmail.com>
2016-02-10 23:12:52 -0500

    Remove unused/commented out lines

--- public/app.js
+++ public/app.js
@@ -29,30 +29,6 @@
         iconSize: [32, 37],
         iconAnchar: [16, 1],
       }
-
-      // leaf_icon: {
-      //   iconUrl: 'examples/img/leaf-green.png',
-      //   shadowUrl: 'examples/img/leaf-shadow.png',
-      //   iconSize:     [38, 95], // size of the icon
-      //   shadowSize:   [50, 64], // size of the shadow
-      //   iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      //   shadowAnchor: [4, 62],  // the same for the shadow
-      //   popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-      // },
-      // div_icon: {
-      //   type: 'div',
-      //   iconSize: [230, 0],
-      //   html: 'Using <strong>Bold text as an icon</strong>: Lisbon',
-      //   popupAnchor:  [0, 0]
-      // },
-      // orange_leaf_icon: {
-      //   iconUrl: 'examples/img/leaf-orange.png',
-      //   shadowUrl: 'examples/img/leaf-shadow.png',
-      //   iconSize:     [38, 95],
-      //   shadowSize:   [50, 64],
-      //   iconAnchor:   [22, 94],
-      //   shadowAnchor: [4, 62]
-      // }
     };
 
     $scope.icons = local_icons;
